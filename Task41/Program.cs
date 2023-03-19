cccc4dd10be9fff7cdbdb4619e9fd025cad105ce
Dmitry-18rus <bgvdima@yandex.ru>
2023-03-19 17:28:35 +0400

    Добавлена задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите величину массива ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [M];
+MetInput(array);
+MetPrint(array);
+MetCount(array);
+
+void MetInput (int [] array)
+{
+    for (int i =0; i<array.Length; i++)
+        array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void MetPrint ( int [] array)
+{
+    Console.Write("[");
+    foreach (int el  in array)
+        Console.Write($"{el} ");
+    // Console.WriteLine();
+    Console.WriteLine("]");
+}
+
+int MetCount(int [] array)
+{
+    int count = 0;
+    for (int i =0; i<array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    Console.WriteLine($"count = {count}");
+return count;
+}
